e31ce27874ae215f8bb29ac4685ec278a5c00b55
Tony Hallett <tonyhallett74@gmail.com>
2018-10-30 18:45:52 +0000

    VerifiableThrowsResult refactor to use AtMost

--- MoqHelpersTests/InSequence/SetUpWrappers/VerifiableWrappers/VerifiableThrowsResultTests.cs
+++ MoqHelpersTests/InSequence/SetUpWrappers/VerifiableWrappers/VerifiableThrowsResultTests.cs
@@ -1,5 +1,4 @@
 using Moq;
-using Moq.Language;
 using Moq.Language.Flow;
 using MoqHelpers.InSequence.SetupWrappers;
 using NUnit.Framework;
@@ -12,33 +11,9 @@ namespace MoqHelpersTests.InSequence.SetUpWrappers.VerifiableWrappers
     {
 
     }
-    
     [TestFixture]
-    internal class VerifiableThrowsResult_AtMost_Tests : VerifiableWrappers_Returns_Tests_Base<IThrowsResult, IVerifies, VerifiableThrowsResult>
+    internal class VerifiableThrowsResult_AtMost_Tests : AtMost_Tests<IThrowsResult, VerifiableThrowsResult>
     {
-        [Test]
-        public void AtMost_Should_Wrap_The_Return_From_Wrapped()
-        {
-            var atMost = 5;
 
-            #pragma warning disable 0618
-            mockWrapped.Setup(m => m.AtMost(atMost)).Returns(mockedWrappedReturn);
-            #pragma warning restore 0618
-            
-            mockVerifiableWrapper.Setup(m => m.WrapVerifiesForVerification(mockedWrappedReturn)).Returns(mockedVerifiableWrapperReturn);
-
-            wrappedReturn = verifiable.AtMost(atMost);
-
-        }
-        [Test]
-        public void AtMostOnce_Should_Wrap_The_Return_From_Wrapped()
-        {
-            #pragma warning disable 0618
-            mockWrapped.Setup(m => m.AtMostOnce()).Returns(mockedWrappedReturn);
-            #pragma warning restore 0618
-            mockVerifiableWrapper.Setup(m => m.WrapVerifiesForVerification(mockedWrappedReturn)).Returns(mockedVerifiableWrapperReturn);
-
-            wrappedReturn = verifiable.AtMostOnce();
-        }
     }
 }
